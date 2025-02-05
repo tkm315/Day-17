1f8207dc9143fa393fac6a5fad624a3d22ea42f8
tkm315 <tahakamalimehraban@gmail.com>
2025-02-05 15:35:34 +0330

    fmt.Fprintf

--- wiki.go
+++ wiki.go
@@ -1,34 +1,25 @@
 package main
 
+//wich one ?
+//fmt.printf or fmt.Fprintf ?
 import (
 	"fmt"
 	"os"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	err := os.WriteFile(filename, p.Body, 0600)
-	return err
-}
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	p := &Page{Title: title, Body: body}
-	return p, nil
-}
-
-// create , save , load
 func main() {
-	p1 := &Page{Title: "TKM315.1", Body: []byte("This is 1st edit")}
-	p1.save()
-	p2, _ := loadPage(p1.Title)
-	fmt.Println(string(p2.Body))
+	myfile, err := os.Create("Fprintf.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer myfile.Close()
+	//look at here
+	fmt.Fprintf(myfile, "this is my %s file", "lovely")
+	readfile, err := os.ReadFile("Fprintf.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(readfile))
 }
