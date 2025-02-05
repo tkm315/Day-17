d7b067822bdce5502642153e354795f5972d95fc
tkm315 <tahakamalimehraban@gmail.com>
2025-02-06 00:01:15 +0330

    valid regular epression

--- wiki.go
+++ wiki.go
@@ -1,12 +1,18 @@
 package main
 
-// template caching
-// line 30 , 37 edited
+// validation
+// line  edited
+// import errors regexp
+// line 43 - 51
+// line 53 - 59
+// line save , view , edit handlers
 import (
+	"errors"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
 
 type Page struct {
@@ -38,9 +44,23 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m != nil {
+		http.NotFound(w, r)
+		return " ", errors.New("invalid page title")
+	}
+	return m[2], nil
+}
+
 // extract title , loadPage(title) , display
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -51,7 +71,10 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 
 // load or create *page , privide editing form
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -61,10 +84,13 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 
 // create *page , find title , find body from form , save page
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
+	err = p.save()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
